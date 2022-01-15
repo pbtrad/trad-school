cbbcd2d4092543101a610688cb542365607f3845
paul <paulbrowne2@gmail.com>
2022-01-15 06:47:55 +0000

    Created membership templates

--- src/tradschool/settings.py
+++ src/tradschool/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'memberships',
     'courses',
 ]
